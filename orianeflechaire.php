caa4eea061a8079bb563ded3ee8b367fa105ce71
natmacira <natimciraolo@gmail.com>
2022-02-23 11:45:10 -0300

    repo creado

---
+++ orianeflechaire.php
@@ -0,0 +1,19 @@
+<?php 
+
+/**
+ * Plugin Name: Oriane Flechaire
+ * Version:     1.0.1
+ * Description: Customizaciones para el sitio orianeflechaire.com
+ * Author:      Natalia Ciraolo
+ * License:     GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+define( 'ORIANE_FLECHAIRE_VERSION', '1.0.1' );
+
+/*
+add_action( 'init', function() {
+    wp_enqueue_style( 'oriane_flechaire_styles', '/wp-content/plugins/orianeflechaire/style.min.css', 
+    array(), ORIANE_FLECHAIRE_VERSION );
+} );
+*/
