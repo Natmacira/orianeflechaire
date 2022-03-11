c206494efe80f08a869359548abbaab327f3747c
natmacira <natimciraolo@gmail.com>
2022-03-11 19:43:43 -0300

    modificaciones staging

--- orianeflechaire.php
+++ orianeflechaire.php
@@ -1,5 +1,4 @@
-<?php 
-
+<?php
 /**
  * Plugin Name: Oriane Flechaire
  * Version:     1.0.1
@@ -11,9 +10,35 @@
 
 define( 'ORIANE_FLECHAIRE_VERSION', '1.0.1' );
 
-/*
+
 add_action( 'init', function() {
     wp_enqueue_style( 'oriane_flechaire_styles', '/wp-content/plugins/orianeflechaire/style.min.css', 
     array(), ORIANE_FLECHAIRE_VERSION );
 } );
-*/
+
+/**
+ * Automatically add product to cart on visit
+ */
+add_action( 'template_redirect', 'add_product_to_cart', 999 );
+
+function add_product_to_cart() {
+	if ( ! is_admin() ) {
+		$product_id = 2449; //replace with your own product id
+		$found = false;
+		//check if product already in cart
+		if ( sizeof( WC()->cart->get_cart() ) > 0 ) {
+			foreach ( WC()->cart->get_cart() as $cart_item_key => $values ) {
+				$_product = $values['data'];
+				if ( $_product->get_id() == $product_id )
+					$found = true;
+			}
+			// if product not found, add it
+			if ( ! $found )
+				WC()->cart->add_to_cart( $product_id );
+		} else {
+			// if no products in cart, add it
+			WC()->cart->add_to_cart( $product_id );
+		}
+	}
+}
+
