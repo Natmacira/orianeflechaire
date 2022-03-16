63ef584a0e10e79716c284cd06be51612289a14a
Lucía <jlmcaceres086@gmail.com>
2022-03-16 11:43:05 -0300

    Se agrega reemplazo de templates de email vía plugin y se añade directorio de templates de emails.

--- orianeflechaire.php
+++ orianeflechaire.php
@@ -1,15 +1,14 @@
 <?php
 /**
  * Plugin Name: Oriane Flechaire
- * Version:     1.0.1
+ * Version:     1.0.2
  * Description: Customizaciones para el sitio orianeflechaire.com
- * Author:      Natalia Ciraolo
+ * Author:      Natalia Ciraolo and Josefina Lucía
  * License:     GPL v2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  */
 
-define( 'ORIANE_FLECHAIRE_VERSION', '1.0.1' );
-
+define( 'ORIANE_FLECHAIRE_VERSION', '1.0.2' );
 
 add_action( 'init', function() {
     wp_enqueue_style( 'oriane_flechaire_styles', '/wp-content/plugins/orianeflechaire/style.min.css', 
@@ -17,28 +16,40 @@ add_action( 'init', function() {
 } );
 
 /**
- * Automatically add product to cart on visit
+ * Automatically add Oriane's book to cart on visit.
  */
 add_action( 'template_redirect', 'add_product_to_cart', 999 );
 
 function add_product_to_cart() {
 	if ( ! is_admin() ) {
-		$product_id = 2449; //replace with your own product id
+		$product_id = 2449; // Oriane's book product ID.
 		$found = false;
-		//check if product already in cart
+		
+		// Check if product already in cart
 		if ( sizeof( WC()->cart->get_cart() ) > 0 ) {
 			foreach ( WC()->cart->get_cart() as $cart_item_key => $values ) {
 				$_product = $values['data'];
 				if ( $_product->get_id() == $product_id )
 					$found = true;
 			}
-			// if product not found, add it
+			// If product not found, add it
 			if ( ! $found )
 				WC()->cart->add_to_cart( $product_id );
 		} else {
-			// if no products in cart, add it
+			// If no products in cart, add it
 			WC()->cart->add_to_cart( $product_id );
 		}
 	}
 }
 
+add_filter( 'woocommerce_locate_template', 'replace_templates_via_plugin', 10, 3 );
+
+function replace_templates_via_plugin( $template, $template_name, $template_path ) {
+	$plugin_path = trailingslashit( plugin_dir_path( __FILE__ ) )  . '/templates/emails/';
+	
+	if ( file_exists( $plugin_path . basename( $template ) ) ){
+		$template = $plugin_path . basename( $template );
+	}
+
+	return $template;
+}
