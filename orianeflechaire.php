d2192dae25c02cba57ed0075d63f9e9acffbf873
natmacira <natimciraolo@gmail.com>
2022-03-18 15:51:34 -0300

    ajustes en el responsive del carrito

--- orianeflechaire.php
+++ orianeflechaire.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * Plugin Name: Oriane Flechaire
- * Version:     1.0.3
+ * Version:     1.0.5
  * Description: Customizaciones para el sitio orianeflechaire.com
  * Author:      Natalia Ciraolo and Josefina Lucía
  * License:     GPL v2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  */
 
-define( 'ORIANE_FLECHAIRE_VERSION', '1.0.3' );
+define( 'ORIANE_FLECHAIRE_VERSION', '1.0.5' );
 
 add_action( 'init', function() {
     wp_enqueue_style( 
